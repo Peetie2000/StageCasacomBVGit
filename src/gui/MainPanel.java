e2d1436ea93c8f3c849f0683b2bf90cc7e986c08
Zomeren <115698@portal.drenthecollege.nl>
2018-01-19 09:41:21 +0100

    excepties toegevoegd

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -4,15 +4,15 @@ import javax.swing.JPanel;
 
 import dataPackage.*;
 
-public class MainPanel extends JPanel implements ButtonListener{
+public class MainPanel extends JPanel implements ButtonListener {
 
 	CSVTable csvTable;
 	private TablePanel table;
 	ButtonPanel buttons;
-	
-	public MainPanel(){
+
+	public MainPanel() {
 		setLayout(null);
-		setSize(800,700);
+		setSize(800, 700);
 		csvTable = new dataPackage.CSVTable();
 
 		buttons = new ButtonPanel(csvTable);
@@ -20,10 +20,10 @@ public class MainPanel extends JPanel implements ButtonListener{
 		buttons.setClickClean(this);
 		buttons.setClickExtra(this);
 		buttons.setLocation(0, 0);
-		
+
 		table = new TablePanel(csvTable);
 		table.setLocation(0, 20);
-		
+
 		add(buttons);
 		add(table);
 		for (int i = 0; i < table.getjTableCsv().getColumnCount(); i++) {
@@ -31,45 +31,47 @@ public class MainPanel extends JPanel implements ButtonListener{
 			table.getjTableCsv().getColumn(columnName).setCellRenderer(new BeginRenderer());
 		}
 	}
-	
-	public void bindCheck(){
+
+	public void bindCheck() {
 		for (int i = 0; i < table.getjTableCsv().getColumnCount(); i++) {
-			String columnName = table.getjTableCsv().getColumnName(i);
-			table.getjTableCsv().getColumn(columnName).setCellRenderer(new CheckRenderer());
+			if (i == 4 || i == 20 || i == 24 || i == 31) {
+			} else {
+				String columnName = table.getjTableCsv().getColumnName(i);
+				table.getjTableCsv().getColumn(columnName).setCellRenderer(new CheckRenderer());
+			}
 		}
 		table.repaint();
 	}
-	
-	public void bindClean(){
+
+	public void bindClean() {
 		for (int i = 0; i < table.getjTableCsv().getColumnCount(); i++) {
 			String columnName = table.getjTableCsv().getColumnName(i);
 			table.getjTableCsv().getColumn(columnName).setCellRenderer(new CleanRenderer());
 		}
 		table.repaint();
 	}
-	
-	public void bindExtra(){
+
+	public void bindExtra() {
 		for (int i = 0; i < table.getjTableCsv().getColumnCount(); i++) {
 			String columnName = table.getjTableCsv().getColumnName(i);
 			table.getjTableCsv().getColumn(columnName).setCellRenderer(new ExtraRenderer());
 		}
 		table.repaint();
 	}
-	
+
 	@Override
 	public void clickCheck() {
 		bindCheck();
 	}
-	
+
 	@Override
 	public void clickClean() {
 		bindClean();
 	}
-	
+
 	@Override
 	public void clickExtra() {
 		bindExtra();
 	}
 
-	
 }
